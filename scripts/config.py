b48d73974dd3cf4eafb2b1e8b3ffd098246e9c4f
lpicci96 <lpicci96@gmail.com>
2021-12-03 10:23:12 -0700

    Create config.py

---
+++ scripts/config.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  3 10:17:41 2021
+
+@author: LucaPicci
+"""
+
+import os
+
+class Paths:
+    def __init__(self, project_dir):
+        self.project_dir = project_dir
+    
+    @property
+    def scripts(self):
+        return os.path.join(self.project_dir, 'scripts')
+
+    
+paths = Paths(os.path.dirname(os.path.dirname(__file__)))
