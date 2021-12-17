274859a1bf5f38bf7812b9fc1e672de4af6100be
lpicci96 <lpicci96@gmail.com>
2021-12-17 15:56:40 -0700

    update

--- scripts/sdr_tracker.py
+++ scripts/sdr_tracker.py
@@ -5,10 +5,120 @@ Created on Fri Dec  3 10:30:41 2021
 @author: LucaPicci
 """
 
-from scripts import config, utils
+from scripts import config, utils, imf
 import pandas as pd
 import numpy as np
+import pandas as pd
+import country_converter as coco
+from typing import Optional
 
+
+# ============================================================================
+# Map Template
+# ============================================================================
+
+def __geometry_df() -> pd.DataFrame:
+    """
+    return a dataframe with iso_code and flourish geometries
+    """
+    g = pd.read_json(config.paths.glossaries + r'/flourish_geometries_world.json')
+    g.columns = ['geo', 'iso_code']
+    g = g.iloc[1:]
+    g = g.drop_duplicates(subset=['iso_code'], keep='first')
+    g = g.set_index('iso_code').T.to_dict('records')[0]
+    df = (pd.DataFrame({'iso_code': g.keys()})
+          .assign(flourish_geom=lambda d: d.iso_code.map(g)))
+
+    return df
+
+def _flourish_map_df(slice_on_column: Optional[str] = None,
+                     slice_by_values: Optional[list] = None) -> pd.DataFrame:
+    """
+    creates a fourish map dataframe template
+    with country name(s) and geometries
+
+        slice_on_column: specify the column to slice
+        slice_by_values: specify the values to slice by
+    """
+    df = pd.merge(__geometry_df(), utils.country_df(),
+                  how='left', on='iso_code')
+
+    if (slice_on_column is not None) & (slice_by_values is not None):
+        df = df[df[slice_on_column].isin(slice_by_values)]
+    elif (slice_on_column is None) & (slice_by_values is not None):
+        raise ValueError('Both parameters must be None or not None')
+    elif (slice_on_column is not None) & (slice_by_values is not None):
+        raise ValueError('Both parameters must be None or not None')
+    else:
+        pass
+
+    return df
+
+
+def africa_map_template() -> None:
+    """
+    Creates template for a flourish map of Africa
+    with country name and geometry
+        output as dataframe saved to glossaries
+    """
+    template = (_flourish_map_df(slice_on_column='continent',
+                                 slice_by_values=['Africa'])
+                .drop(columns='continent'))
+    template.to_csv(f'{config.paths.glossaries}/map_template.csv',
+                    index=False)
+
+
+
+# ============================================================================
+# Get SDR Holdings and allocation from IMF
+# ============================================================================
+#HSA_XDR - SDR allocation
+#HSA_USD
+#RAFASDR_XDR - SDR Holding
+
+def _clean_holdings(df:pd.DataFrame, indicator, title):
+    ''' '''
+    df['REF_AREA'] = coco.convert(df['REF_AREA'])
+    df = df[['TIME_PERIOD', 'OBS_VALUE', 'REF_AREA']]
+    df = df.pivot(index='REF_AREA', columns = 'TIME_PERIOD', values = 'OBS_VALUE')
+    df.columns = df.columns + f'_{title}'
+    df = df.reset_index(drop=False)
+    df.rename(columns = {'REF_AREA':'iso_code'}, inplace=True)
+    
+    return df
+
+def get_holdings(indicator, title, year_month:Optional[str] = None):
+    ''' '''
+    
+    africa = utils.country_df(columns = ['ISO3', 'ISO2', 'continent'])
+    africa = africa[africa.continent == 'Africa']
+    africa = africa.dropna(subset = ['ISO2'])
+    
+    df = imf.get_imf_indicator(country_list = list(africa.ISO2),
+                          database = 'IFS',
+                          frequency = 'M',
+                          indicator = indicator)
+    
+    if year_month is None:
+        df = df.loc[df.TIME_PERIOD == df.TIME_PERIOD.max()]
+    else:
+        df = df.loc[df.TIME_PERIOD == year_month]
+        
+    df = _clean_holdings(df, indicator, title)
+    
+        
+    return df
+
+
+    
+
+
+
+
+
+# ============================================================================
+# Map Build
+# ============================================================================
 
 def read_sheet(grid_number: int) -> pd.DataFrame:
     """
@@ -45,7 +155,8 @@ def popup_html(sdr_df:pd.DataFrame) -> pd.DataFrame:
 
 
 
-def create_sdr() -> None:
+
+def create_sdr_map() -> None:
     """
     creates a csv for flourish map
     """
@@ -56,11 +167,29 @@ def create_sdr() -> None:
     df = pd.merge(map_template, sdr_df, how='left', on='iso_code')
     df = _add_source_html(df, sources_df)
 
-    df['sdrs_received_usd'] = utils.clean_numeric_column(df['sdrs_received_usd'])
+    df['sdrs_allocation_aug_23_usd'] = utils.clean_numeric_column(df['sdrs_allocation_aug_23_usd'])
 
-    df = utils.add_pct_gdp(df, columns=['sdrs_received_usd'],
+
+    df = utils.add_pct_gdp(df, columns=['sdrs_allocation_aug_23_usd'],
                            gdp_year=2021, weo_year=2021, weo_release=2)
-
+    
+    
+    #add holdings
+    cumulative_allocation_sdr = get_holdings('HSA_XDR', 'sdr_allocation_sdr_millions')
+    df = pd.merge(df, cumulative_allocation_sdr, how='left', on = 'iso_code')
+    
+    cumulative_allocation_usd = get_holdings('HSA_USD', 'sdr_allocation_usd_millions')
+    df = pd.merge(df, cumulative_allocation_usd, how='left', on = 'iso_code')
+    
+    sdr_holding_sdr = get_holdings('RAFASDR_XDR', 'sdr_holdings_sdr_millions')
+    df = pd.merge(df, sdr_holding_sdr, how='left', on = 'iso_code')
+    
+    sdr_holding_usd = get_holdings('RAFASDR_USD', 'sdr_holdings_usd_millions')
+    df = pd.merge(df, sdr_holding_usd, how='left', on = 'iso_code')
+    
+    
+    #RAFASDR_XDR - SDR Holding
+    
     df.to_csv(f'{config.paths.output}/sdr.csv',
               index=False)
 
@@ -68,11 +197,20 @@ def create_sdr() -> None:
 
 
 
+
+    
+    
+    
+
+
+
+
 if __name__ == '__main__':
+    pass
 
     # create map template for Africa
-    #map_template.africa_map_template()
+   # africa_map_template()
 
     # create flourish csv
-    create_sdr()
-    print('successfully created map')
+   # create_sdr_map()
+   # print('successfully created map')
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,68 +1,8 @@
-from scripts import config
+from scripts import config, imf
 import weo
 import pandas as pd
-
-
-# ============================================================================
-# WEO tools
-# ============================================================================
-
-
-def _download_weo(year: int,
-                  release: int) -> None:
-    """Downloads WEO as a csv to glossaries folder as "weo_month_year.csv"""
-
-    try:
-        weo.download(year=year,
-                     release=release,
-                     directory=config.paths.glossaries,
-                     filename=f"weo_{year}_{release}.csv")
-    except:
-        raise ConnectionError('Could not download weo data')
-
-
-def _clean_weo(df: pd.DataFrame) -> pd.DataFrame:
-    """cleans and formats weo dataframe"""
-
-    columns = names = {'ISO': 'iso_code',
-                       'WEO Subject Code': 'indicator',
-                       'Subject Descriptor': 'indicator_name',
-                       'Units': 'units',
-                       'Scale': 'scale'}
-    cols_to_drop = ['WEO Country Code', 'Country',
-                    'Subject Notes', 'Country/Series-specific Notes',
-                    'Estimates Start After']
-    df = (df
-          .drop(cols_to_drop, axis=1)
-          .rename(columns=columns)
-          .melt(id_vars=names.values(), var_name='year', value_name='value')
-          )
-    df.value = df.value.map(lambda x: (str(x)
-                                       .strip()
-                                       .replace(',', '')
-                                       .replace('--', '')
-                                       ))
-    df.year = pd.to_numeric(df.year)
-    df.value = pd.to_numeric(df.value, errors='coerce')
-
-    return df
-
-
-def get_gdp(gdp_year:int,
-            weo_year: int,
-            weo_release: int) -> pd.DataFrame:
-    """
-    Retrieves gdp value for a specific year
-    """
-
-    _download_weo(year=weo_year, release=weo_release)
-    df = weo.WEO(f"{config.paths.glossaries}/weo_{weo_year}_{weo_release}.csv").df
-    df = _clean_weo(df)
-    df = df[(df.indicator == 'NGDPD')&(df.year == gdp_year)][['iso_code', 'value']]
-    df.value = df.value*1000000000
-    df.rename(columns = {'value':'gdp'}, inplace=True)
-
-    return df
+from typing import Optional
+import country_converter as coco
 
 
 def add_pct_gdp(df:pd.DataFrame,
@@ -74,12 +14,13 @@ def add_pct_gdp(df:pd.DataFrame,
     adds column(s) to a dataframe with a value as a pct of GDP
     """
 
-    gdp_df = get_gdp(gdp_year, weo_year, weo_release)
+    gdp_df = imf.get_gdp(gdp_year, weo_year, weo_release)
     df = pd.merge(df, gdp_df, on='iso_code', how='left')
     for column in columns:
         df[f'{column}_pct_gdp'] = (df.loc[:, column]/df.loc[:, 'gdp'])*100
 
     return df
+
 
 def clean_numeric_column(column:pd.Series):
     """
@@ -92,3 +33,22 @@ def clean_numeric_column(column:pd.Series):
 
 
 
+def country_df(columns: Optional['list'] = ['ISO3', 'continent'],
+               keep_default_na=False) -> pd.DataFrame:
+    """
+    creates a dataFrame for world country names, codes, regions etc.
+        columns:
+            default ['ISO3', 'continent']
+            additional names available:
+            'name_short', 'name_official', 'ISO2','ISOnumeric','UNcode', 'FAOcode', 'GBDcode',
+            'UNregion', 'EXIO1','EXIO2', 'EXIO3', 'WIOD', 'Eora', 'MESSAGE',
+            'IMAGE', 'REMIND', 'OECD','EU', 'EU28', 'EU27', 'EU27_2007', 'EU25',
+            'EU15', 'EU12', 'EEA','Schengen', 'EURO', 'UN', 'UNmember',
+            'obsolete', 'Cecilia2050', 'BRIC','APEC', 'BASIC', 'CIS',
+            'G7', 'G20', 'IEA', 'regex'
+    """
+
+    df = (pd.read_csv(coco.COUNTRY_DATA_FILE, sep='\t')[columns]
+          .rename(columns={'ISO3': 'iso_code', 'name_short': 'country'}))
+
+    return df
