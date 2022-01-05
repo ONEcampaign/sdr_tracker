755535e43d0e7e665b09a32c0e0fac1507a48cf4
jm-rivera <jorge.rivera@one.org>
2022-01-05 11:26:05 +0100

    Fixes #17
    Fixes #17 by creating a new update file in the root navigation and then getting the action to execute that instead.

---
+++ update.py
@@ -0,0 +1,8 @@
+from scripts import sdr_tracker
+
+if __name__ == '__main__':
+    # create map template for Africa
+    sdr_tracker.create_africa_map_template()
+
+    # create flourish csv
+    sdr_tracker.create_sdr_map()
