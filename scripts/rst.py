e1363748f8e7cd497ad9d599aad8f77660fa6c6c
Luca Picci <lpicci96@gmail.com>
2023-06-16 17:05:35 +0200

    black

--- scripts/rst.py
+++ scripts/rst.py
@@ -11,9 +11,11 @@ from scripts import config
 def _read_rst_data(grid: int) -> pd.DataFrame:
     """Reads RST data from Google Sheets"""
 
-    URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTqLJGWURadUrQeZ-' \
-          'p5iV8ONLptaYjbNVNmzPGpmZhLw0zyCWwuhfu-m1020sxrjkCi11Vm_wFBQ1i6/pub?' \
-          f'gid={grid}&single=true&output=csv'
+    URL = (
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vTqLJGWURadUrQeZ-"
+        "p5iV8ONLptaYjbNVNmzPGpmZhLw0zyCWwuhfu-m1020sxrjkCi11Vm_wFBQ1i6/pub?"
+        f"gid={grid}&single=true&output=csv"
+    )
 
     return pd.read_csv(URL)
 
@@ -23,13 +25,17 @@ def update_rst_timeline_charts():
 
     df = _read_rst_data(1296770218)
     for country in df.country.unique():
-        (df.loc[df['country'] == country]
-         .to_csv(config.Paths.output / 'rst_timeline' /
-                 f'{coco.convert(country, to="ISO3")}.csv', index=False)
-         )
+        (
+            df.loc[df["country"] == country].to_csv(
+                config.Paths.output
+                / "rst_timeline"
+                / f'{coco.convert(country, to="ISO3")}.csv',
+                index=False,
+            )
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     update_rst_timeline_charts()
     with open(config.Paths.output / "rst_updates.csv", "a+", newline="") as write_obj:
         # Create a writer object from csv module
